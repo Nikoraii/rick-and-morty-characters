26eb80488656d0437ec882d8b48606f986dae01f
Nikoraii <kolja.st@gmail.com>
2023-09-25 01:19:02 +0200

    Show characters as a list using ref

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <h1>Test</h1>
+    <li v-for="character in characters" :key="character.id">{{ character.name }}</li>
 </template>
 
 <script>
 import axios from 'axios';
+import { ref } from 'vue';
+
+const characters = ref(null);
 
 export default {
   setup() {
     async function getCharacters() {
-      const characters = await axios.get('https://rickandmortyapi.com/api/character');
-      console.log(characters.data);
+      const response = await axios.get('https://rickandmortyapi.com/api/character');
+      characters.value = response.data.results;
+      console.log(characters);
     }
     
     getCharacters();
+
+    return {
+      characters
+    }
   }
+
 }
 
 </script>
