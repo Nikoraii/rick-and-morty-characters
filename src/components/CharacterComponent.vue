1217f76f710bdd1bd28d8edf29d8cf4028dc9511
Nikoraii <kolja.st@gmail.com>
2023-09-25 01:54:58 +0200

    Added responsive style for character status

--- src/components/CharacterComponent.vue
+++ src/components/CharacterComponent.vue
@@ -3,6 +3,14 @@
     <img class="mx-auto w-full rounded-t-md" :src="character.image">
     <div id="info" class="p-2">
       <h2 class="text-3xl sm:text-2xl lg:text-2xl 2xl:text-3xl font-semibold">{{ character.name }}</h2>
+      <p class="flex items-center capitalize font-semibold card-text">
+        <span class="status-icon inline-block"
+          :class="{
+            'bg-red-800': character.status === 'Dead',
+            'bg-green-600': character.status === 'Alive',
+            'bg-gray-400': character.status === 'unknown'
+          }">
+          </span> {{ character.status }}</p>
     </div>
   </div>
 </template>
