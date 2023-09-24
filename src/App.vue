5425d6c60896784321530309ffed33bdb5d36992
Nikoraii <kolja.st@gmail.com>
2023-09-25 01:11:25 +0200

    Added test function for getting characters from API

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,19 @@
 <template>
   <h1>Test</h1>
 </template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  setup() {
+    async function getCharacters() {
+      const characters = await axios.get('https://rickandmortyapi.com/api/character');
+      console.log(characters.data);
+    }
+    
+    getCharacters();
+  }
+}
+
+</script>
