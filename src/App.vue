20ccbfa824fdba88bba2132fb53f3bc17c62cc65
Nikoraii <kolja.st@gmail.com>
2023-09-25 01:28:33 +0200

    Added infinite scrolling functionality

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>Test</h1>
-  <ul>
+  <ul ref="scrollComponent">
     <li v-for="character in characters" :key="character.id">
       <h1>{{ character.name }}</h1>
     </li>
@@ -8,25 +8,60 @@
 </template>
 
 <script>
+import { ref, onMounted, onUnmounted } from 'vue';
 import axios from 'axios';
-import { ref } from 'vue';
 
 const characters = ref(null);
 
 export default {
   setup() {
-    async function getCharacters() {
-      const response = await axios.get('https://rickandmortyapi.com/api/character');
-      characters.value = response.data.results;
+    const scrollComponent = ref(null);
+
+    onMounted(async () => {
+      try {
+        characters.value = await getCharacters();
+      } catch (error) {
+        console.error(error);
+      }
+      window.addEventListener("scroll", handleScroll);
+    });
+
+    onUnmounted(() => {
+      window.removeEventListener("scroll", handleScroll);
+    });
+
+    const loadMore = async () => {
+      try {
+        let newChars = await getCharacters();
+        characters.value.push(...newChars);
+      } catch(error) {
+        console.error('No more characters!');
+      }
     }
-    
-    getCharacters();
+
+    const handleScroll = () => {
+      let element = scrollComponent.value;
+      if (element.getBoundingClientRect().bottom <= window.innerHeight) {
+        loadMore();
+      }
+    }
+
+    let api = 'https://rickandmortyapi.com/api/character';
+
+    const getCharacters = async () => {
+        const characters = await axios.get(api);
+        try {
+            api = characters.data.info.next;
+            return characters.data.results;
+        } catch(error) {
+            console.error(error);
+        }
+    }
 
     return {
-      characters
+      characters,
+      scrollComponent
     }
   }
-
-}
-
+};
 </script>
