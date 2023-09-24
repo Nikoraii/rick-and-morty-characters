f4914d521135d23be291526dd14efc76a30a5e49
Nikoraii <kolja.st@gmail.com>
2023-09-25 01:53:26 +0200

    Added responsive design for card title

--- src/components/CharacterComponent.vue
+++ src/components/CharacterComponent.vue
@@ -2,7 +2,7 @@
   <div class="bg-slate-800 rounded-md text-slate-50 border border-gray-500">
     <img class="mx-auto w-full rounded-t-md" :src="character.image">
     <div id="info" class="p-2">
-      <h2 class="text-3xl">{{ character.name }}</h2>
+      <h2 class="text-3xl sm:text-2xl lg:text-2xl 2xl:text-3xl font-semibold">{{ character.name }}</h2>
     </div>
   </div>
 </template>
