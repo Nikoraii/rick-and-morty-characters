90c624a1d2f7aa58c7056a82d31b345ec0bbc590
Nikoraii <kolja.st@gmail.com>
2023-09-25 01:52:24 +0200

    Removed v-for from component

---
+++ src/components/CharacterComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="bg-slate-800 rounded-md text-slate-50 border border-gray-500">
+    <img class="mx-auto w-full rounded-t-md" :src="character.image">
+    <div id="info" class="p-2">
+      <h2 class="text-3xl">{{ character.name }}</h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        character: Object
+    }
+}
+</script>
