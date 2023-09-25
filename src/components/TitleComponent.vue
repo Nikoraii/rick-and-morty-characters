422a6f3f98bd2390e24bc9eac774c8cf0650cd5f
Nikoraii <kolja.st@gmail.com>
2023-09-25 02:02:16 +0200

    Added styles and content to the TitleComponent

--- src/components/TitleComponent.vue
+++ src/components/TitleComponent.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>Title</h1>
+  <div id="title" class="text-center text-white mb-2">
+    <h1 class="text-3xl sm:text-4xl xl:text-5xl">List of the Rick and Morty characters</h1>
+    <p class="text-sm sm:text-base">Made using <a href="https://rickandmortyapi.com/" target="_blank" class="text-sky-600 underline font-semibold decoration-gray-400 underline-offset-2">The Rick and Morty API</a></p>
+  </div>
 </template>
